a2666319d332d53aa77b54c7dd88d45766203bc4
Nikki <abarcanikki@gmail.com>
2020-07-29 15:13:23 +0800

    Updates

---
+++ precache-manifest.8aa181a3b7b4d6e2870ae00538a42f49.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a05059b9704a4107b5bb0204096b7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba4d96fdbcf6adfa547b",
+    "url": "/static/css/main.82280b58.chunk.css"
+  },
+  {
+    "revision": "17f318ccbff1bd371e3d",
+    "url": "/static/js/2.32eebdc2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.32eebdc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba4d96fdbcf6adfa547b",
+    "url": "/static/js/main.10843b25.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ef578019656c11deb882011ea9c017e7",
+    "url": "/static/media/banner.ef578019.jpg"
+  },
+  {
+    "revision": "df18bb001582ca2ee59a6da9b113da86",
+    "url": "/static/media/cat-1.df18bb00.png"
+  },
+  {
+    "revision": "cc340faf9b4b2131013d8dfd96f9e636",
+    "url": "/static/media/cat-4.cc340faf.png"
+  },
+  {
+    "revision": "188da2930833120b0069e6116e577107",
+    "url": "/static/media/prod-1.188da293.jpg"
+  },
+  {
+    "revision": "b5041fb2074b8d74d1430e4d31e2bf9b",
+    "url": "/static/media/prod-2.b5041fb2.jpg"
+  },
+  {
+    "revision": "0b407d75086000a23a8976c31098bbc1",
+    "url": "/static/media/prod-25.0b407d75.jpg"
+  },
+  {
+    "revision": "d884cfa0c00e3a7e1836da1e8f9ee4a3",
+    "url": "/static/media/prod-26.d884cfa0.jpg"
+  },
+  {
+    "revision": "3268a32f1f7419098bf51afd15f10085",
+    "url": "/static/media/prod-27.3268a32f.jpg"
+  },
+  {
+    "revision": "c8d290a32b4f1598002bbf632c15c960",
+    "url": "/static/media/prod-28.c8d290a3.jpg"
+  },
+  {
+    "revision": "d690343d5902ac471e756ab4600efe29",
+    "url": "/static/media/prod-29.d690343d.jpg"
+  },
+  {
+    "revision": "166cc6ce147c74fd938b93c4d04979a8",
+    "url": "/static/media/prod-3.166cc6ce.jpg"
+  },
+  {
+    "revision": "18727b06271785c09f03dee4f54c0568",
+    "url": "/static/media/prod-30.18727b06.jpg"
+  },
+  {
+    "revision": "10774a12c72cb04d87bcd6bdeced01f7",
+    "url": "/static/media/prod-31.10774a12.jpg"
+  },
+  {
+    "revision": "313c85169e8b196cd7e4f1ddddb9b4d1",
+    "url": "/static/media/prod-32.313c8516.jpg"
+  },
+  {
+    "revision": "4f60473429c72966013e65e33892513b",
+    "url": "/static/media/prod-4.4f604734.jpg"
+  },
+  {
+    "revision": "81c994a206cbe77b6af8d2f268ccfa49",
+    "url": "/static/media/prod-5.81c994a2.jpg"
+  }
+]);
